0c50841837296dce8fe697c165f2049b19f78cb5
danisayumin <sayuminitta@gmail.com>
2022-03-30 12:13:12 -0300

    Create ex008.js
    velocidade do carro/condicionais

---
+++ Aula11/ex008.js
@@ -0,0 +1,7 @@
+var vel = 12
+console.log (`A velocidade do seu carro é ${vel}`)
+if (vel > 60) { //condição simples
+      console.log ('voce ultrapassou a velocidade permitida ,MULTADO!!!')
+      
+}
+console.log ('dirija sempre usando cinto de segurança!')
